701dfd8b28cb929fe64844a041f0b3caa633cd79
Patilnpriya <53858776+Patilnpriya@users.noreply.github.com>
2020-09-23 17:06:10 +0530

    udated name

--- _example/example.tf
+++ _example/example.tf
@@ -1,11 +1,11 @@
 module "labels" {
   source = "https://github.com/Patilnpriya/terraform-labels"
 
-  Application_id        = var.app_id
-  Application_name      = var.app_name
-  Platform_name         = var.platform
-  OWNER                 = var.squadname
-  Environment           = var.env
-  Managed_by            = var.managedby
-  label_order = ["Application_id", "Application_name","Platform_name","OWNER","Environment","Managed_by"]
+  application_id        = var.app_id
+  application_name      = var.app_name
+  platform_name         = var.platform
+          owner         = var.squadname
+  environment           = var.env
+  managed_by            = var.managedby
+  label_order = ["application_id", "application_name","platform_name","owner","environment","managed_by"]
 }
